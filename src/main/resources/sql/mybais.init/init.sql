ef4befe323e9ae6cd82f2ee6cbf9c47b1c95ca47
wangjianjun <jianjun_wang@andall.com>
2020-10-09 16:33:27 +0800

    源码分析

---
+++ src/main/resources/sql/mybais.init/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `t_demo` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `name` varchar(32) DEFAULT NULL,
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COMMENT='dmeo';
